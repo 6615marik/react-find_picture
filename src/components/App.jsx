a721da1fb10cc624ce8e11b1b80ec6eb3de52cc7
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2023-03-15 17:36:44 +0200

    \\

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,34 @@
 import { Component } from 'react';
 import { SerchBar } from './Searchbar/Searchbar';
-import { fethAPI } from './Searchbar/fethAPI.1';
+// import { fethAPI } from './Searchbar/fethAPI.1';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
 export class App extends Component {
   state = {
-    inputValue: '',
+    inputValue: 'car',
     hits: [],
     error: '',
     page: 1,
   };
 
   componentDidUpdate(_, prevState) {
-    const { inputValue, page } = this.state;
-    if (prevState.inputValue !== inputValue || prevState.page !== page) {
-      fethAPI(inputValue)
+    const { inputValue } = this.state;
+    if (prevState.inputValue !== inputValue) {
+      console.log(inputValue);
+      return fetch(
+        `https://pixabay.com/api/?q=${inputValue}&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(
+            new Error(`Not picture with name ${inputValue}`)
+          );
+        })
         .then(hits => this.setState({ hits }))
-        .catch(error => console.log(error));
+        .catch(error => this.setState({ error }));
     }
   }
 
--- src/components/Searchbar/fethAPI.jsx
+++ src/components/Searchbar/fethAPI.jsx
@@ -1,11 +1,11 @@
-export const fethAPI = inputValue => {
-  console.log(inputValue);
-  return fetch(
-    `https://pixabay.com/api/?key=32372176-cc8c36cdf5846484fc1786588&q=${inputValue}&image_type=photo`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
+// export const fethAPI = inputValue => {
+//   console.log(inputValue);
+//   return fetch(
+//     `https://pixabay.com/api/?key=32372176-cc8c36cdf5846484fc1786588&q=yellow+flowers&image_type=photo`
+//   ).then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     return Promise.reject(new Error(`Not picture with name ${inputValue}`));
+//   });
+// };
